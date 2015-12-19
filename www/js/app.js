f572d7a91c332c5dd145fcd9cb13dee8253eee95
ptri7957 <phillip.trinh1@gmail.com>
2015-12-19 23:27:46 +1100

    First Commit

---
+++ www/js/app.js
@@ -0,0 +1,42 @@
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(pedometer) {
+        console.log("Available!");
+    }else{
+        console.log("Not available...");
+    }
+  });
+}).controller('PedometerCtrl', ['$scope', function($scope){
+    // Success handler for starting pedometer updates
+    var successHandler = function(pedometerData){
+        
+        // Grab the number of steps taken, and start and end dates
+        $scope.numberOfSteps = pedometerData.numberOfSteps;
+        $scope.startDate = pedometerData.startDate;
+        $scope.endDate = pedometerData.endDate;
+        document.getElementById("results").innerHTML = endDate;
+    };
+    
+    // The error to throw if updating pedometer data fails
+    var onError = function(error){
+        console.log("Error: ", error);
+    };
+    
+    // Start pedometer updates
+    $scope.startPedometer = function(){
+        pedometer.startPedometerUpdates(successHandler, onError);
+        document.getElementById("recording").innerHTML = "Recording...";
+    };
+    
+    // Stop pedometer updates
+    $scope.stopPedometer = function(){
+        pedometer.stopPedometerUpdates(function(success){
+            console.log(success);
+        }, function(error){
+            console.log(error);
+        });
+        document.getElementById("recording").innerHTML = "Recording finished.";
+    };
+}]);
