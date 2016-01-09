dbaaf620749db20a59f767364dcab582fe20a3eb
Phillip <phillip.trinh1@gmail.com>
2016-01-09 12:35:17 +1100

    Formatted timestamp

--- www/js/history.js
+++ www/js/history.js
@@ -20,11 +20,21 @@ $('document').ready(function(){
                 steps.className = "col";
                 steps.innerHTML = snap.val().steps;
 
-                var startDate = new Date(parseInt(snap.val().start_date) * 1000);
-                var endDate = new Date(parseInt(snap.val().end_date) * 1000);
+                var startDate = new Date(parseInt(snap.val().start_date));
+                var endDate = new Date(parseInt(snap.val().end_date));
                 var duration = document.createElement('div');
                 duration.className = "col";
-                duration.innerHTML = endDate.getHours() - startDate.getHours();
+                var durationS = ((endDate.getTime() - startDate.getTime())/1000);
+                var durationM = ((endDate.getTime() - startDate.getTime())/1000/60);
+                var durationH = ((endDate.getTime() - startDate.getTime())/1000/60/60);
+                if(duration <= 60){
+                  duration.innerHTML = durationS + "Seconds";
+                }else if(duration >= 60 && duration <= 3600){
+                  duration.innerHTML = durationM + "Minutes";
+                }else{
+                  duration.innerHTML = durationH + "Hours";
+                }
+                
                         
                 row.appendChild(name);
                 row.appendChild(steps);
